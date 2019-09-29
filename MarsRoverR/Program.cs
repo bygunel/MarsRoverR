6b6d6ba3a062815d8245a3b76e43e52a48328ba7
bygunel <bygunel.necdet@gmail.com>
2019-09-29 14:43:12 +0300

    Add project files.

---
+++ MarsRoverR/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MarsRoverR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot robot = new Robot();
+            Console.WriteLine("Input:");
+            string temporaryValue = Console.ReadLine();
+            string[] temporaryValue2 = temporaryValue.Split(' ');
+            robot.UpperRightCoordinateX = Convert.ToInt32(temporaryValue2[0]);
+            robot.UpperRightCoordinateY = Convert.ToInt32(temporaryValue2[1]);
+
+            AGAIN:
+            temporaryValue = Console.ReadLine();
+            temporaryValue2 = temporaryValue.Split(' ');
+            robot.CurrentLocationX = Convert.ToInt32(temporaryValue2[0]);
+            robot.CurrentLocationY = Convert.ToInt32(temporaryValue2[1]);
+            robot.CurrentDirection = Convert.ToChar(temporaryValue2[2]);
+
+            string returnValue = robot.WhereIsRobot(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine("Output:");
+            Console.WriteLine(returnValue);
+            Console.WriteLine("==================\n");
+
+
+
+            goto AGAIN;
+        }
+    }
+}
---
+++ MarsRoverR/Robot.cs
@@ -0,0 +1,116 @@
+﻿namespace MarsRoverR
+{
+    class Robot
+    {
+        #region RobotStatus
+        private int currentLocationX = 1;
+        private int currentLocationY = 1;
+        private char currentDirection = 'N';
+
+        public int CurrentLocationX { get => currentLocationX; set => currentLocationX = value; }
+        public int CurrentLocationY { get => currentLocationY; set => currentLocationY = value; }
+        public char CurrentDirection { get => currentDirection; set => currentDirection = value; }
+        #endregion
+        #region AreaInformation
+        private int upperRightCoordinateX;
+        private int upperRightCoordinateY;
+
+        public int UpperRightCoordinateX { get => upperRightCoordinateX; set => upperRightCoordinateX = value; }
+        public int UpperRightCoordinateY { get => upperRightCoordinateY; set => upperRightCoordinateY = value; }
+
+        #endregion
+
+
+        private bool InTheMap(int CoordinateX, int CoordinateY)
+        {
+            if (CoordinateX > upperRightCoordinateX || CoordinateY > upperRightCoordinateY)
+                return false;
+
+            return true;
+        }
+        private void NextMove(char nextMove)
+        {
+            // Left
+            if (nextMove == 'L')
+            {
+                switch (currentDirection)
+                {
+                    case 'E':
+                        currentDirection = 'N';
+                        break;
+                    case 'N':
+                        currentDirection = 'W';
+                        break;
+                    case 'W':
+                        currentDirection = 'S';
+                        break;
+                    case 'S':
+                        currentDirection = 'E';
+                        break;
+                }
+                return;
+            }
+            // Right
+            if (nextMove == 'R')
+            {
+                switch (currentDirection)
+                {
+                    case 'E':
+                        currentDirection = 'S';
+                        break;
+                    case 'S':
+                        currentDirection = 'W';
+                        break;
+                    case 'W':
+                        currentDirection = 'N';
+                        break;
+                    case 'N':
+                        currentDirection = 'E';
+                        break;
+                }
+                return;
+            }
+            // Move
+            switch (currentDirection)
+            {
+                case 'E':
+                    currentLocationX++;
+                    break;
+                case 'W':
+                    currentLocationX--;
+                    break;
+                case 'N':
+                    currentLocationY++;
+                    break;
+                case 'S':
+                    currentLocationY--;
+                    break;
+            }
+        }
+
+
+
+
+
+        public string WhereIsRobot(string movementDirectives)
+        {
+            if (!InTheMap(currentLocationX, currentLocationY))
+                return "First location is NOT valid !!!";
+
+
+            int step = 0;
+            foreach (char operation in movementDirectives)
+            {
+                step++;
+                NextMove(operation);
+
+                if (!InTheMap(currentLocationX, currentLocationY))
+                    return string.Format("{0}. step operation invalid !!!", step);
+            }
+
+
+            return string.Format("{0} {1} {2}", currentLocationX, currentLocationY, currentDirection);
+
+        }
+    }
+}
